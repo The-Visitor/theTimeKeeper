0f6d8b0eb47d5bd18079659fff6bf32e7ef7d0ef
Pallav <pallav@email.com>
2018-08-20 23:35:01 +0530

    started writing scripts and inputs

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,29 @@ $(document).ready(function()
     $('select').formSelect();
     $('.timepicker').timepicker();
 
-    $("#findMeTimeButton").on('click', function()
+    $("#letsStart").on('click', function()
     {
       var name = $("#guestName").val();
       var totalHours = $("#totalHours").val();
       var totalTasks = $("#totalTasks").val();
       var wakeUp = $("#wakeUpTime").val();
-      $("#userName").append(name);
-      var j = 1;
-      for(var i=0;i<totalTasks;i++)
-      {
-        var str1 = "<li id='select-options-7c623b3d-ebda-50df-b952-5b3b3f019385"+i+"' tabindex='0' class='selected'><span>"+(i+1)+"</span></li>";
-        var str2 = "<option value = '"+(i+1)+"'>"+(i+1)+"</option>";
-        $("#importantTasksSelect").parent().find("ul").append(str1);
-        $("#importantTasksSelect").append(str2);
-      }
+      var importantTasks = $("#importantTasks").val();
+      var optionalTasks = $("#optionalTasks").val();
+      var chillHours = $("#chillHours").val();
+      var breakTime = $("#breakTime").val();
+      $("#importantTaskDiv").empty();
+      $("#optionalTaskDiv").empty();
+          for(var i =0;i<importantTasks;i++)
+          {
+            var str = "<div class='card taskCard'><div class='card-image waves-effect waves-block waves-light' style='margin-bottom:30px'><img class='activator' src='images/work.png' style='height: 200px;width: 200px; padding: 10px; margin: auto'></div><a class='btn-floating activator halfway-fab waves-effect waves-light red'><i class='material-icons right'>add</i></a><div class='card-content'><span class='card-title activator grey-text text-darken-4'><input style='background : white; padding-left: 10px;'  type='text' placeholder='Task name'><p><input type='number' style='background:white; padding-left: 10px;' placeholder='Time needed to complete it'></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>Any extra info<i class='material-icons right'>close</i></span><p></p></div></div>";
+            $("#importantTaskDiv").append(str);
+          }
+          for(var i =0;i<optionalTasks;i++)
+          {
+            var str = "<div class='card taskCard'><div class='card-image waves-effect waves-block waves-light' style='margin-bottom:30px'><img class='activator' src='images/work.png' style='height: 200px;width: 200px; padding: 10px; margin: auto'></div><a class='btn-floating activator halfway-fab waves-effect waves-light red'><i class='material-icons right'>add</i></a><div class='card-content'><span class='card-title activator grey-text text-darken-4'><input style='background : white; padding-left: 10px;'  type='text' placeholder='Task name'><p><input type='number' style='background:white; padding-left: 10px;' placeholder='Time needed to complete it'></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>Any extra info<i class='material-icons right'>close</i></span><p></p></div></div>";
+            $("#optionalTaskDiv").append(str);
+          }
+      
+      
     });
   });
